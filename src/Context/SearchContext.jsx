824d24f3eb8e392fdb1350bca43da2efdeb4488d
abdallahhesham <abdallahhesham2@gmail.com>
2022-11-28 11:25:43 +0200

    Children to children

--- src/Context/SearchContext.jsx
+++ src/Context/SearchContext.jsx
@@ -37,7 +37,7 @@ const MediaContextProvider = (props) => {
   
   return <SearchContext.Provider value={{search,clearData,setmovieName,movieName}}>
 
-{props.Children}
+{props.children}
   </SearchContext.Provider>
 }
 
